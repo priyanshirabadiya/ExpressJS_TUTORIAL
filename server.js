d421579ad85e9f518707a4d733a87e7455c8dd38
priyanshirabadiya <rabadiyapriyansi@gmail.com>
2024-08-08 18:32:04 +0530

    Lec-3

--- server.js
+++ server.js
@@ -1,45 +1,29 @@
-// CRUD OPERATION / get and send methods
+// Middleware 
 
 const express = require('express');
 const server = express();
 
+const muFun = (req, res, next) => {
+    if (req.query.age >= '18') {
+        console.log('Success');
+        next();
+    }
+    else {
+        res.json({ message: "Sorry there is an issue..." });
+    }
+}
 
-const fs = require('fs');
-const data = fs.readFileSync('./dummy.js' , 'utf-8');
+server.use(muFun);   // application
 
-// POST , GET , PUT , PATCH , DELETE method
-
-server.get('/' , (req,res) => {
-    res.write("<h1>Hello this is home page</h1>");
-    res.end();
+server.get("/", (req, res) => {
+    res.end("Welcome to express js page")
 })
 
-server.get('/about' , (req,res) => {
-    res.end("<h1>This is ABOUT page</h1>");
+server.post("/login", (req, res) => {
+    res.send("<h1>Post send method</h1>")
 })
 
-// POST METHOD
-
-server.post('/',(req,res) => {
-    res.send('<h1>POST method</h1>');    // send methods
+server.listen(1122, () => {
+    console.log('Server started at http://localhost:1122');
 })
 
-// PUT / PATCH UPDATING METHOD
-
-server.put("/" , (req,res) => {
-    res.json({message:"Hello this is put method."});
-})
-
-server.patch("/" , (req,res) => {
-    res.status(400);   // default 200 for success 
-    res.json({message : "hello patch method is called."});
-})
-
-server.delete('/' , (req,res) => {
-    res.write(data);
-    res.end();  // must need to end()
-})
-
-server.listen(1111,() => {
-    console.log('Server start at http://localhost:1111');
-})
