4ddd4dd821600ae60526888d77b3597491acb4ed
priyanshirabadiya <rabadiyapriyansi@gmail.com>
2024-08-08 18:21:46 +0530

    Creating server

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-// console.log("sdfsjdgfjsdgfj");
+// Create server
 
-// const express = require('express');
-// const app = express();
-// app.get('/' , (req,res) => {
-//     res.setHeader('Content-type' , 'text/html');
-//     res.write('<h1>This is home page.</h1>');
-//     res.end();
-// })
+console.log("Hello World");
 
-// app.listen(5555,() => {
-//     console.log('Server at http://localhost:5555');
-// })
+const express = require('express');
+const app = express();
+app.get('/' , (req,res) => {
+    res.setHeader('Content-type' , 'text/html');
+    res.write('<h1>This is home page.</h1>');
+    res.end();
+})
 
+app.listen(5555,() => {
+    console.log('Server at http://localhost:5555');
+})
 
 const express = require('express');
 const server = express();
@@ -26,9 +27,17 @@ server.get('/user' , (req,res) => {
     res.setHeader('Content-type' , 'text/html' );
     res.write("<h1>This is user page for checking nodemon.</h1>");
     res.end();
-} )
+});
 
 server.listen(2222,() => {
     console.log('server at http://127.0.0.1:2222');    
 })
+
+
+
+
+
+
+
+
 
